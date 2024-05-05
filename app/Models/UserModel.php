cfc8bfef5ba76d3da6d6b289b6ce6489d7e9841f
Dayutirta <dayumahara502@gmail.com>
2024-05-05 23:11:01 +0700

    Percobaan RESTFUL API

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,22 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
+// use Illuminate\Contracts\Auth\Authenticatable;
+// use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+// use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model implements Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use AuthenticatableTrait;
+
+    // use AuthenticatableTrait;
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }    
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
+
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
     protected $fillable = ['level_id','username','nama','password'];
 
-    public function level() : BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class,'level_id','level_id');
-    }
+    // public function level() : BelongsTo
+    // {
+    //     return $this->belongsTo(LevelModel::class,'level_id','level_id');
+    // }
 }
 
